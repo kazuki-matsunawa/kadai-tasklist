d10a6e33ab3abe006a7e0a98c4705b033e62c669
kazuki.matsunawa <s.matsunwa.k@hotmail.com>
2020-03-03 13:47:31 +0000

    CRUD messages

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>メッセージ一覧</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>メッセージ</th>
+                    <th>ステータス</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{{ $task->id }}</td>
+                    <td>{{ $task->content }}</td>
+                    <td>{{ $task->status }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+
+@endsection
