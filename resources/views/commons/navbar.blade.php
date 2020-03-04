d32b9eac33855756a53efa41005d2c29019b6d5f
kazuki.matsunawa <s.matsunwa.k@hotmail.com>
2020-03-04 10:54:25 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -11,7 +11,7 @@
             <ul class="navbar-nav">
                 <li class="nav-item">{!! link_to_route('tasks.create', '新規メッセージの投稿', [], ['class' => 'nav-link']) !!}</li>
                 <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
             </ul>
         </div>
     </nav>
